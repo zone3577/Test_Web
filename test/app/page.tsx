cb84df82236981bec1840140acf606f0d1ff1781
zone3577 <74754452+zone3577@users.noreply.github.com>
2025-09-15 08:16:44 +0000

    Next.js

---
+++ test/app/page.tsx
@@ -0,0 +1,9 @@
+import Homeone from '@/components/homeone/homeone';
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <Homeone />
+    </div>
+  );
+}
